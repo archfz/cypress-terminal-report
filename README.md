704f49fe5cd284dbd9c1bdd3a2f3272e667b7052
Fodor Zoltan <fodor18zoltan@gmail.com>
2020-01-21 21:47:11 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# cypress-terminal-report
-Better terminal output for failed cypress tests.
+# Cypress terminal report
+
+Plugin for cypress that adds better terminal output when tests fail
+on the terminal for better debugging. Prints cy commands, console.warn 
+and console.error from the browser to terminal when a test fails. 
+
+![demo](https://raw.githubusercontent.com/archfz/cypress-terminal-report/demo.png)
+
+## Install
+
+1. Install npm package.
+    ```bash
+    npm i --save-dev cypress-terminal-report
+    ```
+2. Register the output plugin in `cypress/plugins/index.js`
+    ```js
+    module.exports = (on) => {
+       require('cypress-terminal-report').installPlugin(on);
+    };
+    ```
+3. Register the log collector support in `cypress/support/index.js`
+    ```js
+    require('cypress-terminal-report').installSupport();
+    ```
+
+## Options
+
+The plugin install can receive some options: `.installPlugin(on, options)`:
+- `options.defaultTrimLength` - default: 200; max length of cy.log and console.warn/console.error.
+- `options.commandTrimLength` - default: 600; max length of cy commands.
---
+++ index.js
@@ -0,0 +1,123 @@
+function pipeLogsToTerminal() {
+  let oldConsoleWarn;
+  let oldConsoleError;
+  let logs = [];
+
+  Cypress.on('window:before:load', () => {
+    const docIframe = window.parent.document.querySelector("[id*='Your App']");
+    const appWindow = docIframe.contentWindow;
+
+    oldConsoleWarn = appWindow.console.warn;
+    oldConsoleError = appWindow.console.error;
+
+    appWindow.console.warn = (...args) => {
+      logs.push(['warn', args.join(' ')]);
+      oldConsoleWarn(...args);
+    };
+    appWindow.console.error = (...args) => {
+      logs.push(['error', args.join(' ')]);
+      oldConsoleError(...args);
+    };
+  });
+
+  Cypress.Commands.overwrite('log', (subject, ...args) => {
+    logs.push(['cy:log', args.join(' ')]);
+    subject(...args);
+  });
+
+  Cypress.on('log:added', options => {
+    if (options.instrument === 'command' && options.consoleProps) {
+      if (options.name === 'log' || (options.name === 'task' && options.message.match(/terminalLogs/))) {
+        return;
+      }
+      let detailMessage = '';
+      if (options.name === 'xhr') {
+        detailMessage = (options.consoleProps.Stubbed === 'Yes' ? 'STUBBED ' : '') + options.consoleProps.Method + ' ' + options.consoleProps.URL;
+      }
+      const log = options.name + '\t' + options.message + (detailMessage !== '' ? ' ' + detailMessage : '');
+      logs.push(['cy:command', log])
+    }
+  });
+
+  Cypress.mocha.getRunner().on('test', () => {
+    logs = [];
+  });
+
+  afterEach(function () {
+    if (this.currentTest.state !== 'passed') {
+      cy.task('terminalLogs', logs);
+    }
+  });
+}
+
+function nodeAddLogsPrinter(on, options = {}) {
+  const chalk = require('chalk');
+
+  on('task', {
+    terminalLogs: (messages) => {
+      messages.forEach(([type, message], i) => {
+        let color,
+          typeString,
+          processedMessage = message,
+          trim = options.defaultTrimLength || 200,
+          icon = '⚠';
+
+        if (type === 'warn') {
+          color = 'yellow';
+          typeString = '       cons.warn ';
+        } else if (type === 'error') {
+          color = 'red';
+          typeString = '      cons.error ';
+        } else if (type === 'cy:log') {
+          color = 'green';
+          typeString = '          cy:log ';
+          icon = 'ⓘ';
+        } else if (type === 'cy:command') {
+          typeString = '      cy:command ';
+          color = 'green';
+          icon = '✔';
+          trim = options.commandTrimLength || 600;
+        }
+
+        if (i === messages.length - 1) {
+          color = 'red';
+          icon = '✘'
+        }
+
+        if (message.length > trim) {
+          processedMessage = message.substring(0, trim) + " ...";
+        }
+
+        console.log(chalk[color](typeString + icon + ' '), processedMessage);
+      });
+
+      console.log('\n\n');
+      return null;
+    },
+  });
+}
+
+module.exports = {
+  /**
+   * Installs the cypress plugin for printing logs to terminal.
+   *
+   * Needs to be added to plugins file.
+   *
+   * @param {Function} on
+   *    Cypress event listen handler.
+   * @param {object} options
+   *    Options for displaying output:
+   *      - defaultTrimLength?: Trim length for console and cy.log.
+   *      - commandTrimLength?: Trim length for cy commands.
+   */
+  installPlugin: (on, options= {}) =>
+    nodeAddLogsPrinter(on, options),
+
+  /**
+   * Installs the logs collector for cypress.
+   *
+   * Needs to be added to support file.
+   */
+  installSupport: () =>
+    pipeLogsToTerminal(),
+};
