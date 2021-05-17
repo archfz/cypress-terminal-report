c667f1d5723adbe1555cb79e653d75bd5fa0f793
Fodor Zoltan <fodor18zoltan@gmail.com>
2021-05-17 14:41:30 +0300

    Prepare for circle ci.

--- test/bin/split_matrix_specs.sh
+++ test/bin/split_matrix_specs.sh
@@ -4,6 +4,9 @@
 allSpecs=$(find specs/ -name "*.spec.js" | sort -n)
 specCount=$(echo "$allSpecs" | wc -l)
 
+CI_NODE_TOTAL=$CIRCLE_NODE_TOTAL
+CI_NODE_INDEX=$CIRCLE_NODE_INDEX
+
 if [ "$CI_NODE_INDEX" == "" ]; then
   CY_SPECS=$(echo "$allSpecs" | paste -sd " " -)
 else
