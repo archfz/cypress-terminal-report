3e7b3385dc1c86e652467bca0c40e875fe1e23ce
blake.vandercar <blake.vandercar@validatek.com>
2022-04-12 11:08:42 -0600

    better comment

--- src/outputProcessor/BaseOutputProcessor.d.ts
+++ src/outputProcessor/BaseOutputProcessor.d.ts
@@ -1,8 +1,13 @@
 /**
  * Gives the functions and variables available for use when specifying a custom output processor.
- * i.e. Allows use of this.writeSpecChunk without indicating ts error
+ * i.e. Allows use of this.writeSpecChunk without ts warning/error
  * 
  * Example usage:
+ * 
+ * import { AllMessages } from 'cypress-terminal-report/src/installLogsPrinter'
+ * import { BaseOutputProcessor } from 'cypress-terminal-report/src/outputProcessor/BaseOutputProcessor'
+ * 
+ * ...
  * 
  * outputTarget: {
  *     'custom|cts':  function outputProcessor(this: BaseOutputProcessor, messages: AllMessages){ <custom output processor function> }                
