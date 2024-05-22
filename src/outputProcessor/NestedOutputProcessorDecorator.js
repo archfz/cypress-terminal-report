f4eeede44369e01712b7d4bbcf0b9bc747ebd466
Johannes Munk <j.munk@klarso.com>
2024-05-22 10:35:54 +0200

    NestedOutputProcessorDecorator (feat): outPath pattern with tokens [relpath] [basename]
    outputTarget: {
         '*|[relpath]/[basename].txt': function () {..}
    }

--- src/outputProcessor/NestedOutputProcessorDecorator.js
+++ src/outputProcessor/NestedOutputProcessorDecorator.js
@@ -3,9 +3,16 @@ const path = require('path');
 module.exports = class NestedOutputProcessorDecorator {
 
   constructor(filePattern, specRoot, decoratedFactory) {
-	const parts = filePattern.split('|');
-    this.root = parts[0];
-    this.ext = parts[1];
+    const parts = filePattern.split('|');
+	// for pattern matching: first part of filePattern has to start with '*|'
+	if (parts[0] === "*") {
+		// join all parts, but remove '|'
+		this.pattern = filePattern.substring(2).split('|').join('');
+	} else {
+		// legacy format: path-prefix|extension
+		this.pattern = parts[0]+"/[relpath]/[basename]."+parts[1];
+	}
+
     this.specRoot = specRoot || '';
     this.decoratedFactory = decoratedFactory;
 
@@ -22,7 +29,18 @@ module.exports = class NestedOutputProcessorDecorator {
     }
 
     const relativeSpec = path.relative(this.specRoot, spec);
-    const outPath = path.join(this.root, relativeSpec.replace(new RegExp(path.extname(relativeSpec) + '$'), `.${this.ext}`));
+	
+	const parsed = path.parse(relativeSpec);
+	const relpath = parsed.dir;
+	const basename = parsed.name;
+
+	// replace [*] tokens in pattern to build output filepath
+	var outPath = this.pattern.replace(/\[([^\]]+)\]/g, function(m, cap1) {
+		if (cap1 === "relpath") return relpath;
+		if (cap1 === "basename") return basename;
+		return "-";
+	});
+
     const processor = this.decoratedFactory(outPath);
 
     processor.initialize();
