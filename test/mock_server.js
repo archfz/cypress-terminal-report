c467ca039f935e4299da573cb6f2d979b4124537
Fodor Zoltan <fodor18zoltan@gmail.com>
2024-09-14 17:29:38 +0300

    Fix backwards compatibility test.

--- test/mock_server.js
+++ test/mock_server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/v3/4b2d23ec-4516-4a94-967e-995596d01a32', (req, res) => {
   res.status(500).send('Hey ya! Great to see you here. Btw, nothing is configured for this request path. Create a rule and start building a mock API.');
 })
 
-app.post('/v3/57a00707-bccf-4653-ac50-ba1c00cad431', (req, res) => {
-  res.status(400).send({
+const handle57 = (status) => (req, res) => {
+  res.status(status).send({
     "status": "Wrong!",
     "data": {
       "corpo": "corpo da resposta",
       "titulo": "titulo da resposta"
     }
   })
-})
+};
+app.post('/v3/57a00707-bccf-4653-ac50-ba1c00cad431', handle57(400))
+app.get('/v3/57a00707-bccf-4653-ac50-ba1c00cad431', handle57(200))
 
 app.listen(3015);
-console.log('Mock server listening on port 3000');
+console.log('Mock server listening on port 3015');
