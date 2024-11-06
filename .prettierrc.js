95e29fc0c0f1d73eabad354a6744288a3d2b8193
Blake Vandercar <blake.vandercar@validatek.com>
2024-11-05 19:03:30 -0700

    format md file

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,7 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
 module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
@@ -5,4 +9,13 @@ module.exports = {
   printWidth: 100,
   bracketSpacing: false,
   semi: true,
+  overrides: [
+    {
+      files: ['*.md'],
+      options: {
+        trailingComma: 'none',
+        semi: false,
+      },
+    },
+  ],
 };
