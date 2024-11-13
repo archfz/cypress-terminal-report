591c3303b058ef70514e4e061513b550b5b959d3
Blake Vandercar <blake.vandercar@validatek.com>
2024-11-13 11:01:04 -0700

    imrpoved test

--- test/specs/utils.spec.js
+++ test/specs/utils.spec.js
@@ -15,6 +15,8 @@ describe('utils', () => {
                ['___text text___', 'BItext textIB'],
                ['__*text text*__', 'BItext textIB'],
                ['*__text text__*', 'IBtext textBI'],
+               ['text text __text text__', 'text text Btext textB'],
+               ['_text text_ __text text__', 'Itext textI Btext textB'],
                ['_text text_text_', 'Itext textItext_'],
                ['text text_','text text_'],
                ['**text **text**', 'Btext Btext**'],
