d9710a131a54d8001b2eb1d8015a73dfaa4dd2a0
Blake Vandercar <blake.vandercar@validatek.com>
2024-11-13 14:39:16 -0700

    revert

--- src/outputProcessor/JsonOutputProcessor.ts
+++ src/outputProcessor/JsonOutputProcessor.ts
@@ -10,7 +10,6 @@ export default class JsonOutputProcessor extends BaseOutputProcessor implements 
     Object.entries(allMessages).forEach(([spec, tests]) => {
       let data: Record<string, Record<string, Log[]>> = {[spec]: {}};
 
-      
       Object.entries(tests).forEach(([test, messages]) => {
         data[spec][test] = messages.map((message) => {
           const {timeString, ...data} = message;
