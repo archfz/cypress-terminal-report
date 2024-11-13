bf84798fc0245fa0e58d57fecd992cc2429ab1ee
Blake Vandercar <blake.vandercar@validatek.com>
2024-11-12 18:05:39 -0700

    create internal global types file

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,3 @@
-/**
- * This file used for adding types to package interfaces that are not documented
- * by the package (not included in the package's types).
- */
-
 declare namespace Cypress {
   import {Log} from "./src/installLogsCollector";
   import {BuiltinOutputProcessorsTypes} from "./src/types";
@@ -12,47 +7,5 @@ declare namespace Cypress {
       getLogs(format: BuiltinOutputProcessorsTypes): string | null;
       getLogs(format?: 'none' = 'none'): Log[] | null;
     }
-
-    onSpecReady(...args: any[]): void;
-
-    mocha: {
-      getRunner(): Mocha.Runner
-      getRootSuite(): Mocha.Suite
-    }
-
-    state(key: 'done', callback: (error: Error) => Promise<void>): void
-    state(key: 'error', error: Error): void
-  }
-
-  interface Chainable<Subject = any> extends Pick<Promise<Subject>, 'catch'> {
-  }
-}
-
-declare namespace Mocha {
-  interface InvocationDetails {
-    relativeFile?: string
-    fileUrl?: string
-  }
-
-  interface Hook {
-    hookName: string
-    _ctr_hook: boolean
-  }
-
-  interface Runnable {
-    hookName: string
-    invocationDetails: InvocationDetails
-    id: string
-    order: unknown
-    wallClockStartedAt: unknown
-    timings: unknown
-  }
-
-  interface Suite {
-    invocationDetails: InvocationDetails
-  }
-
-  interface Test {
-    failedFromHookId?: unknown
   }
 }
