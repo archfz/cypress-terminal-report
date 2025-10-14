5ec7001b6b97c65a443f95a135f12909a1b1a5ed
Fodor Zoltan <fodor18zoltan@gmail.com>
2025-10-14 13:04:37 +0300

    Format.

--- test/cypress/integration/memoryCleanup.spec.js
+++ test/cypress/integration/memoryCleanup.spec.js
@@ -1,18 +1,18 @@
 describe('Trigger issue:', () => {
   describe('tests that take', () => {
     it('1 second', () => {
-      cy.wait(1000)
-    })
-  })
+      cy.wait(1000);
+    });
+  });
   describe('more tests that take', () => {
     it('10 seconds', () => {
-      cy.wait(10000)
-    })
+      cy.wait(10000);
+    });
     it('15 seconds', () => {
-      cy.wait(15000)
-    })
+      cy.wait(15000);
+    });
     it('10 seconds', () => {
-      cy.wait(10000)
-    })
-  })
-})
+      cy.wait(10000);
+    });
+  });
+});
